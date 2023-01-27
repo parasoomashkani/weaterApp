1c6d024d00d6e6f16261b93b4798def1ed593ac0
parasoomashkani <122097128+parasoomashkani@users.noreply.github.com>
2023-01-27 23:11:41 +0330

    added new codes

--- index.js
+++ index.js
@@ -22,31 +22,34 @@ function formatDate(timestamp) {
     let day = days[date.getDay()];
     return `${day} ${hours}:${minutes}`;
   }
-  function displayForecast(){
-    let forecast=document.querySelector("#forecast");
-    let forecastHTML="";
-forecastElement.innerHTML=`
-<div class="row">
-<div class="col-2">
-        <div class="weather-forecast-date">
-                Thu
+  function displayForecast() {
+    let forecastElement = document.querySelector("#forecast");
+  
+    let days = ["Thu", "Fri", "Sat", "Sun"];
+  
+    let forecastHTML = `<div class="row">`;
+    days.forEach(function (day) {
+      forecastHTML =
+        forecastHTML +
+        `
+        <div class="col-2">
+          <div class="weather-forecast-date">${day}</div>
+          <img
+            src="http://openweathermap.org/img/wn/50d@2x.png"
+            alt=""
+            width="42"
+          />
+          <div class="weather-forecast-temperatures">
+            <span class="weather-forecast-temperature-max"> 18° </span>
+            <span class="weather-forecast-temperature-min"> 12° </span>
+          </div>
         </div>
-        <img src="https://ssl.gstatic.com/onebox/weather/48/sunny.png" alt=""
-        width="56"/>
-        <div class="weather-forecast-temperature">
-                <span class="weather-forecaste-temperature-max">
-                        10
-                </span>
-                /
-                <span class="weather-forecast-temperature-min">
-                        12
-                </span>
-        </div>
-</div>
-</div>
-
-`
-
+    `;
+    });
+  
+    forecastHTML = forecastHTML + `</div>`;
+    forecastElement.innerHTML = forecastHTML;
+    console.log(forecastHTML);
   }
 
 function displayTemperature(response){
@@ -122,3 +125,7 @@ fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+
+
+displayForecast();
